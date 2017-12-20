b844d0cd43dace64e09f950574f5925cba638fca
Andrew Zubko <dev@zubko.biz>
2017-12-20 12:38:54 +0200

    fix copyright text

--- src/util.cpp
+++ src/util.cpp
@@ -889,7 +889,7 @@ std::string CopyrightHolders(const std::string& strPrefix)
 
     // Check for untranslated substitution to make sure Bitcoin Core copyright is not removed by accident
     if (strprintf(COPYRIGHT_HOLDERS, COPYRIGHT_HOLDERS_SUBSTITUTION).find("Bitcoin Core") == std::string::npos) {
-        strCopyrightHolders += "\n" + strPrefix + "The Bitcoin Core developers";
+        strCopyrightHolders += "\n Copyright (C) 2009-2017 The Bitcoin Core developers";
     }
     return strCopyrightHolders;
 }
